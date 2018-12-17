c256fdeb2698a84fc9e29c3ccd9f142d592bed8f
bpdnd <3438291751@qq.comgit config --global user.email 3438291751@qq.com>
2018-12-17 17:11:37 +0800

    按钮

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void  hehe(View view){
+        TextView textView = (TextView)findViewById(R.id.wjk);
+        textView.setVisibility(View.GONE);
+
+        Button button = (Button) view;
+        String buttonStr =  button.getText().toString();
+        Log.d("23",buttonStr);
+        if (buttonStr.equals("按钮")){
+           Log.d("1", "23");
+        }
     }
 }
